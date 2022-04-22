08051e1e832a80c17a0e055bc8631e1c8c400bbc
Remember remember the fifth of november <Henrika2001@hotmail.com>
2022-04-22 22:01:22 +0200

    Update Minesweeper.py
    ikke farlige områder reveales nå, men discoverymetoden for dette er wonky

--- Minesweeper.py
+++ Minesweeper.py
@@ -1,34 +1,111 @@
-import pygame
-from pygame.locals import *
+import pygame 
 import random
 
 board = []
 boardpos = []
+k=6
 
-for x in range(0,64):
-    z = random.randint(0,6)
-    if z == 2:
+for x in range(0,256):
+    z = random.randint(0,k)
+    if z == 1 or 0:
         board.append("*")
+        k=3
     else:
         board.append(" ")
+        k=4
+
 
 
 def game(screen):
     screen.fill((180, 180, 180))
     n = 40
-    z=0
     for x in range(0,16):
         for z in range(0,16):
-            pygame.draw.rect(screen, (145, 145, 145), pygame.Rect(5+n*z, 75+n*x, n, n))
-            boardpos.append((5+n*z, 75+n*x))
+            boardpos.append(pygame.draw.rect(screen, (145, 145, 145), pygame.Rect(5+n*z, 75+n*x, n, n)))
     for x in range(0,17):
         for z in range(0,17):
             pygame.draw.line(screen, (40, 40, 40), (5+n*z,75+n*x), (5+n*9,75+n*x), 2)
             pygame.draw.line(screen, (40, 40, 40), (5+n*z,75+n*x), (5+n*z,75+n*9), 2)
+    pygame.display.flip()
     
             
 
+def loosegame(screen):
+    screen.fill((180, 180, 180))
+    n = 40
+    fontgrid = pygame.font.Font('freesansbold.ttf', 40)
+    p=0
+    for x in range(0,16):
+        for z in range(0,16):
+            boardpos.append(pygame.draw.rect(screen, (145, 145, 145), pygame.Rect(5+n*z, 75+n*x, n, n)))
+            k = fontgrid.render(str(board[p]), True, (164, 126, 115))   
+            screen.blit(k, (20+n*z, 90+n*x))
+            p+=1
+    for x in range(0,17):
+        for z in range(0,17):
+            pygame.draw.line(screen, (40, 40, 40), (5+n*z,75+n*x), (5+n*9,75+n*x), 2)
+            pygame.draw.line(screen, (40, 40, 40), (5+n*z,75+n*x), (5+n*z,75+n*9), 2)
+    pygame.display.flip()
 
+
+def showsquares(screen, rect):
+    pygame.draw.rect(screen, (200, 200, 200), rect)
+    revealem(screen,rect)
+    pygame.display.flip()
+
+def revealem(screen, rect):
+    k = boardpos.index(rect)
+    if board[k-1] != "*":
+        a=True
+        while a:
+            if board[k-1] != "*":
+                pygame.draw.rect(screen, (200, 200, 200), boardpos[k-1])
+                k-=1
+            elif k>3:
+                a = False
+            else:
+                a=False
+        k = boardpos.index(rect)
+    if board[k+1]!="*":
+        a=True
+        while a:
+            if board[k+1] != "*":
+                pygame.draw.rect(screen, (200, 200, 200), boardpos[k+1])
+                k+=1
+            elif k>3:
+                a = False
+            else:
+                a=False
+        k = boardpos.index(rect)
+    if board[k+16]!="*":
+        a=True
+        while a:
+            if board[k+16] != "*":
+                pygame.draw.rect(screen, (200, 200, 200), boardpos[k+16])
+                k+=16
+            elif k>3:
+                a = False
+            else:
+                a=False
+        k = boardpos.index(rect)
+    if board[k-16]!="*":
+        a=True
+        while a:
+            if board[k-16] != "*":
+                pygame.draw.rect(screen, (200, 200, 200), boardpos[k-16])
+                k-=16
+            elif k>3:
+                a = False
+            else:
+                a=False
+        k = boardpos.index(rect)
+            
+
+
+    pygame.display.flip()
+
+
+    
 
 
 
@@ -48,8 +125,11 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             Lastclick = event.pos
             print(Lastclick)
-           """ y=0
             for x in boardpos:
                 if x.collidepoint(Lastclick):
-                    print(board[y])
-                y+=1"""
+                    if board[boardpos.index(x)]=="*":
+                        loosegame(screen)
+                        break
+                    else:
+                        showsquares(screen, x)
+                        
